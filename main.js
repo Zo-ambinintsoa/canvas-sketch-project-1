580140e3dec556d6b6a4543282cec796b569ad5d
Ambinintsoa Herizo <nambinintsoa577@gmail.com>
2023-06-19 01:26:17 -0700

    adding comment and documentation to the code

--- main.js
+++ main.js
@@ -3,120 +3,156 @@ const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
 const Colors = require('canvas-sketch-util/color');
 const risoColors = require("riso-colors");
+
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [2048, 2048]
 };
 
+/**
+ * Main sketch function that generates the animated sketch
+ * @param {Object} context - The canvas rendering context
+ * @param {number} width - The width of the canvas
+ * @param {number} height - The height of the canvas
+ */
 const sketch = ({ context, width, height }) => {
-  // random.setSeed('domestika')
-  let x, y, w, h,fill ,stroke , blend ;
-  const num = 50 ;
-  const degree = -30;
-  const rects = [];
-  let shadowColor
-  const rectColor = [
+  // Set up variables for rectangle properties, colors, and mask
+  let rectX, rectY, rectWidth, rectHeight, rectFill, rectStroke, rectBlend;
+  const numRectangles = 50;
+  const rotationDegree = -30;
+  const rectangles = [];
+  let shadowColor;
+  const rectangleColors = [
     random.pick(risoColors),
     random.pick(risoColors),
     random.pick(risoColors)
   ];
-  const bgColor = random.pick(risoColors).hex
+  const backgroundColor = random.pick(risoColors).hex;
   const mask = {
     radius: width * 0.4,
     sides: 3,
     x: width * 0.5,
     y: height * 0.58
+  };
+
+  // Create random rectangles and store them in the rectangles array
+  for (let i = 0; i < numRectangles; i++) {
+    rectX = random.range(0, width);
+    rectY = random.range(0, height);
+    rectWidth = random.range(200, 600);
+    rectHeight = random.range(40, 200);
+
+    rectFill = random.pick(rectangleColors).hex;
+    rectStroke = random.pick(risoColors).hex;
+    rectBlend = (random.value() > 0.5) ? 'overlay' : 'source-over';
+
+    rectangles.push({ x: rectX, y: rectY, width: rectWidth, height: rectHeight, stroke: rectStroke, fill: rectFill, blend: rectBlend });
   }
-  for (let i = 0; i < num; i++) {
-    x = random.range(0, width);
-    y = random.range(0, height);
-    w = random.range(200, 600);
-    h = random.range(40, 200);
 
-    fill = random.pick(rectColor).hex;
-    stroke = random.pick(risoColors).hex;
-    blend = (random.value() > 0.5) ? 'overlay' : 'source-over'
-  
-    rects.push({x, y, w, h, stroke, fill, blend})  
-    }
+  /**
+   * Render function that is called for each frame of the animation
+   * @param {Object} context - The canvas rendering context
+   * @param {number} width - The width of the canvas
+   * @param {number} height - The height of the canvas
+   */
   return ({ context, width, height }) => {
-    context.fillStyle = bgColor;
+    // Set the background color
+    context.fillStyle = backgroundColor;
     context.fillRect(0, 0, width, height);
-  
+
+    // Save the context state
     context.save();
 
+    // Apply the mask by clipping the drawing area
     context.translate(mask.x, mask.y);
-    drawPolygone({context, radius: mask.radius, sides: mask.sides})
+    drawPolygon({ context, radius: mask.radius, sides: mask.sides });
     context.clip();
-    
 
+    // Iterate over the rectangles array and draw each rectangle
+    rectangles.forEach(rectangle => {
+      const { x, y, width, height, stroke, fill, blend } = rectangle;
 
-    rects.forEach( rect => {
-    const { x, y, w, h, stroke , fill, blend} = rect;
-    context.save();
-    context.translate(- mask.x, - mask.y);
-     
-    context.translate(x, y);
-    context.lineWidth=10;
-    
-    context.globalCompositeOperation=blend;
-    context.strokeStyle = stroke;
-    context.fillStyle= fill;
-    
+      context.save();
+      context.translate(-mask.x, -mask.y);
+      context.translate(x, y);
 
-    drawSkewedRect({context, w, h, degree})
+      // Set styles for the rectangle
+      context.lineWidth = 10;
+      context.globalCompositeOperation = blend;
+      context.strokeStyle = stroke;
+      context.fillStyle = fill;
+
+      drawSkewedRect({ context, width, height, degree: rotationDegree });
+
+      // Set shadow color and offset
       shadowColor = Colors.offsetHSL(fill, 0, 0, -20);
       shadowColor.rgba[3] = 0.5;
-    context.shadowColor= Colors.style(shadowColor.rgba);
-    context.shadowOffsetX=-10;
-    context.shadowOffsetY=20;
-    
-    context.fill();
+      context.shadowColor = Colors.style(shadowColor.rgba);
+      context.shadowOffsetX = -10;
+      context.shadowOffsetY = 20;
 
-    context.shadowColor= null;
-    context.stroke();
+      // Fill the rectangle
+      context.fill();
 
-    context.globalCompositeOperation='source-over';
-      context.lineWidth=2;
-      context.strokeStyle='black';
+      // Reset shadow settings and stroke the rectangle
+      context.shadowColor = null;
       context.stroke();
-    context.restore();
+
+      // Reset global composite operation and stroke style
+      context.globalCompositeOperation = 'source-over';
+      context.lineWidth = 2;
+      context.strokeStyle = 'black';
+      context.stroke();
+
+      context.restore();
     });
+
     context.restore();
-    
+
+    // Draw another polygon shape with a different blending mode
     context.save();
     context.translate(mask.x, mask.y);
-    context.lineWidth=15;
-
-    drawPolygone({context, radius: mask.radius - context.lineWidth, sides: mask.sides})
-    context.globalCompositeOperation='color-burn';
-    
-    context.strokeStyle=rectColor[0].hex;
+    context.lineWidth = 15;
+    drawPolygon({ context, radius: mask.radius - context.lineWidth, sides: mask.sides });
+    context.globalCompositeOperation = 'color-burn';
+    context.strokeStyle = rectangleColors[0].hex;
     context.stroke();
     context.restore();
-    
   };
 };
 
-const drawSkewedRect = ({context , w = 600, h = 200, degree = -45}) => {
+/**
+ * Function to draw a skewed rectangle
+ * @param {Object} context - The canvas rendering context
+ * @param {number} width - The width of the rectangle
+ * @param {number} height - The height of the rectangle
+ * @param {number} degree - The angle of rotation in degrees
+ */
+const drawSkewedRect = ({ context, width = 600, height = 200, degree = -45 }) => {
   const angle = math.degToRad(degree);
-  const rx = Math.cos(angle) * w;
-  const ry = Math.sin(angle) * w;
-  
-  context.save();
-  context.translate(rx* -0.5, (ry + h) * -0.5);
-  
-    context.beginPath();
-    context.moveTo(0,0);
-    context.lineTo(rx,ry);
-    context.lineTo(rx ,ry + h);
-    context.lineTo(0,h);
-    context.closePath();
-    context.stroke();
-  
-  context.restore();
+  const rx = Math.cos(angle) * width;
+  const ry = Math.sin(angle) * width;
 
-}
-const drawPolygone = ({context, radius = 100, sides = 3}) => {
+  context.save();
+  context.translate(rx * -0.5, (ry + height) * -0.5);
+
+  context.beginPath();
+  context.moveTo(0, 0);
+  context.lineTo(rx, ry);
+  context.lineTo(rx, ry + height);
+  context.lineTo(0, height);
+  context.closePath();
+  context.stroke();
+
+  context.restore();
+};
+
+/**
+ * Function to draw a polygon shape
+ * @param {Object} context - The canvas rendering context
+ * @param {number} radius - The radius of the polygon
+ * @param {number} sides - The number of sides of the polygon
+ */
+const drawPolygon = ({ context, radius = 100, sides = 3 }) => {
   const slice = Math.PI * 2 / sides;
   context.beginPath();
   context.moveTo(0, -radius);
@@ -126,4 +162,6 @@ const drawPolygone = ({context, radius = 100, sides = 3}) => {
   }
   context.closePath();
 };
+
+// Start the animation
 canvasSketch(sketch, settings);
