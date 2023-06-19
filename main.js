3db7ec587ad31afdc6b958c4dbec2025e40a5d1f
Ambinintsoa Herizo <nambinintsoa577@gmail.com>
2023-06-19 03:32:47 -0700

    adding random form

--- main.js
+++ main.js
@@ -9,93 +9,106 @@ const settings = {
 };
 
 const sketch = () => {
-  const triangleCount = 40;
-  const triangleSize = 50;
+  const shapeCount = 40;
+  const shapeMinSides = 3;
+  const shapeMaxSides = 9;
+  const shapeSize = 50;
 
-  const triangles = [];
+  const shapes = [];
 
-  // Generate random triangles
-  for (let i = 0; i < triangleCount; i++) {
-    const x = random.range(triangleSize, settings.dimensions[0] - triangleSize);
-    const y = random.range(triangleSize, settings.dimensions[1] - triangleSize);
+  // Generate random shapes
+  for (let i = 0; i < shapeCount; i++) {
+    const x = random.range(shapeSize, settings.dimensions[0] - shapeSize);
+    const y = random.range(shapeSize, settings.dimensions[1] - shapeSize);
+    const sides = random.range(shapeMinSides, shapeMaxSides + 1);
     const directionX = random.pick([-1, 1]);
     const directionY = random.pick([-1, 1]);
     const color = random.pick(risoColors).hex;
-    let rotationSpeed = random.range(0.1, 0.5);
+    const isSpinning = false;
+    const rotationSpeed = random.range(0.1, 0.5);
 
-    triangles.push({
+    shapes.push({
       x,
       y,
+      sides,
       directionX,
       directionY,
       color,
-      isSpinning: false,
+      isSpinning,
       rotation: 0,
       rotationSpeed,
     });
   }
 
-  const drawTriangle = (context, triangle) => {
-    const { x, y, directionX, directionY, color, isSpinning, rotation, rotationSpeed } = triangle;
+  const drawShape = (context, shape) => {
+    const { x, y, sides, directionX, directionY, color, isSpinning, rotation, rotationSpeed } = shape;
 
     // Update x position based on direction
     const speed = 100;
     const deltaX = directionX * speed / settings.dimensions[0];
-    triangle.x += deltaX;
+    shape.x += deltaX;
 
-    // Reverse direction if triangle hits the canvas borders on the x-axis
-    if (triangle.x < triangleSize || triangle.x > settings.dimensions[0] - triangleSize) {
-      triangle.directionX *= -1;
+    // Reverse direction if shape hits the canvas borders on the x-axis
+    if (shape.x < shapeSize || shape.x > settings.dimensions[0] - shapeSize) {
+      shape.directionX *= -1;
     }
 
     // Update y position based on direction
     const deltaY = directionY * speed / settings.dimensions[1];
-    triangle.y += deltaY;
+    shape.y += deltaY;
 
-    // Reverse direction if triangle hits the canvas borders on the y-axis
-    if (triangle.y < triangleSize || triangle.y > settings.dimensions[1] - triangleSize) {
-      triangle.directionY *= -1;
+    // Reverse direction if shape hits the canvas borders on the y-axis
+    if (shape.y < shapeSize || shape.y > settings.dimensions[1] - shapeSize) {
+      shape.directionY *= -1;
     }
 
     // Apply spinning rotation if enabled
     if (isSpinning) {
-      triangle.rotation += rotationSpeed;
+      shape.rotation += rotationSpeed;
     }
 
-    // Draw triangle
-    const halfSize = triangleSize / 2;
+    // Draw shape
+    const radius = shapeSize / 2;
+    const angle = (Math.PI * 2) / sides;
+
     context.save();
     context.translate(x, y);
     context.rotate(rotation);
     context.beginPath();
-    context.moveTo(0, -halfSize);
-    context.lineTo(halfSize, halfSize);
-    context.lineTo(-halfSize, halfSize);
+    context.moveTo(radius, 0);
+
+    for (let i = 1; i < sides; i++) {
+      const x = radius * Math.cos(angle * i);
+      const y = radius * Math.sin(angle * i);
+      context.lineTo(x, y);
+    }
+
     context.closePath();
 
-    // Set triangle fill style
+    // Set shape fill style
     context.fillStyle = color;
     context.fill();
 
     context.restore();
   };
 
-  const handleTriangleClick = (event) => {
+  const handleShapeClick = (event) => {
     const { offsetX, offsetY } = event;
 
-    triangles.forEach((triangle) => {
-      const { x, y, isSpinning } = triangle;
-      const halfSize = triangleSize / 2;
+    shapes.forEach((shape) => {
+      const { x, y, isSpinning } = shape;
+      const radius = shapeSize / 2;
 
-      // Check if the click is inside the triangle
+      // Check if the click is inside the shape
       if (
-        offsetX >= x - halfSize &&
-        offsetX <= x + halfSize &&
-        offsetY >= y - halfSize &&
-        offsetY <= y + halfSize
+        offsetX >= x - radius &&
+        offsetX <= x + radius &&
+        offsetY >= y - radius &&
+        offsetY <= y + radius
       ) {
-        triangle.color = random.pick(risoColors).hex; // Change color when clicked
-        triangle.isSpinning = !isSpinning; // Toggle spinning state
+        shape.color = random.pick(risoColors).hex; // Change color when clicked
+        shape.isSpinning = !isSpinning; // Toggle spinning state
+        shape.rotation = 0; // Reset rotation
       }
     });
   };
@@ -105,11 +118,11 @@ const sketch = () => {
     context.clearRect(0, 0, width, height);
 
     // Add event listener to canvas
-    context.canvas.addEventListener('click', handleTriangleClick);
+    context.canvas.addEventListener('click', handleShapeClick);
 
-    // Draw triangles
-    triangles.forEach((triangle) => {
-      drawTriangle(context, triangle);
+    // Draw shapes
+    shapes.forEach((shape) => {
+      drawShape(context, shape);
     });
   };
 };
